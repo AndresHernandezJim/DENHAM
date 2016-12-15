434087d49d119b9d045287b3ce5dbeed74230d79
wixis93 <luis.panchis@gmail.com>
2016-12-15 07:32:59 -0600

    querysde vistas

---
+++ database/views/venta_categoria_view.sql
@@ -0,0 +1,9 @@
+INSERT INTO `venta_categoria_view` VALUES (1, 'Beverages', 9532);
+INSERT INTO `venta_categoria_view` VALUES (4, 'Dairy Products', 9149);
+INSERT INTO `venta_categoria_view` VALUES (3, 'Confections', 7906);
+INSERT INTO `venta_categoria_view` VALUES (8, 'Seafood', 7681);
+INSERT INTO `venta_categoria_view` VALUES (8, 'Others', 7681);
+INSERT INTO `venta_categoria_view` VALUES (2, 'Condiments', 5298);
+INSERT INTO `venta_categoria_view` VALUES (5, 'Grains/Cereals', 4562);
+INSERT INTO `venta_categoria_view` VALUES (6, 'Meat/Poultry', 4199);
+INSERT INTO `venta_categoria_view` VALUES (7, 'Produce', 2990);
